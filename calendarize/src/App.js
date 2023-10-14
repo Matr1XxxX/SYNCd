6976d45a3322273ea32ff89f7138ad3d8c825e2d
Jayanth Ramesh <jayanthramesh16.jr@gmail.com>
2023-10-14 14:02:00 +0530

    all changes

--- calendarize/src/App.js
+++ calendarize/src/App.js
@@ -1,11 +1,17 @@
+import { icon } from '@fortawesome/fontawesome-svg-core/import.macro'
+import './App.css';
+import './calander.js'
+import ReactDOM from 'react-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUser } from '@fortawesome/free-solid-svg-icons'
+
 function App() {
   return (
-    <navigator className="navbar">
-      <h1>CALENDARIZE</h1>
-      <button>Add Event</button>
-      <button>Profile</button>
+    <navigator className="navBar">
+      <h1 className="nameLogo">CALENDARIZE</h1>
+      <button className="addEvent">Add Event</button>
+      <button className="profile"><FontAwesomeIcon icon={faUser} /></button>
     </navigator>
   );
 }
-
 export default App;
